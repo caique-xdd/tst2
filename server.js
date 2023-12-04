e4cd5c08cdfe92c88a0b5a541dccf38083c2cc04
caique-xdd <caiquedecarvalho990@gmail.com>
2023-12-04 20:00:43 -0300

    tst servidor

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen( process.env.PORT ?? 3334,
+    ()=> process.env.PORT? console.log(process.env.PORT) :  console.log('Servidor ON na porta 3334')
+);
